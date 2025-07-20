df23bacc57d1e5ad3c92b23eea520ef5632cc47e
a91shah <17akshayshah@gmail.com>
2025-07-20 18:12:59 +0530

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -1,38 +1,24 @@
-
 import streamlit as st
-import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
-# Define the required scopes for accessing Google Sheets
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-# Google Sheet ID
-SHEET_ID = "1V61WCd-bGiLTwrdQKr1WccSIEZ570Ft9WLPdlxdiVJA"
-
-# Function to authenticate and connect to the Google Sheet
 def connect_to_gsheet():
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
-    client = gspread.authorize(creds)
-    sheet = client.open_by_key(SHEET_ID).sheet1
-    return sheet
-
-# Function to load inventory data into a pandas DataFrame
-def load_inventory_from_gsheet(sheet_id, sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["gcp_service_account"], scope
+    )
     client = gspread.authorize(creds)
-    sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
+    return client
+
+def load_inventory_from_gsheet(sheet_id, worksheet_name):
+    client = connect_to_gsheet()
+    sheet = client.open_by_key(sheet_id).worksheet(worksheet_name)
     data = sheet.get_all_records()
     return pd.DataFrame(data)
 
-
-# Function to update the Google Sheet with the modified DataFrame
-def update_inventory_to_gsheet(sheet_id, sheet_name, df):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
-    client = gspread.authorize(creds)
-    sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
+def update_inventory_to_gsheet(sheet_id, worksheet_name, df):
+    client = connect_to_gsheet()
+    sheet = client.open_by_key(sheet_id).worksheet(worksheet_name)
     sheet.clear()
     sheet.update([df.columns.values.tolist()] + df.values.tolist())
-
