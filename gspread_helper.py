eb4df187a20454e03b995759058f1438bcd1ebd4
a91shah <17akshayshah@gmail.com>
2025-07-20 18:06:30 +0530

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -18,14 +18,21 @@ def connect_to_gsheet():
     return sheet
 
 # Function to load inventory data into a pandas DataFrame
-def load_inventory_from_gsheet():
-    sheet = connect_to_gsheet()
+def load_inventory_from_gsheet(sheet_id, sheet_name):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
     data = sheet.get_all_records()
-    df = pd.DataFrame(data)
-    return df
+    return pd.DataFrame(data)
+
 
 # Function to update the Google Sheet with the modified DataFrame
-def update_inventory_to_gsheet(df):
-    sheet = connect_to_gsheet()
+def update_inventory_to_gsheet(sheet_id, sheet_name, df):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
     sheet.clear()
     sheet.update([df.columns.values.tolist()] + df.values.tolist())
+
