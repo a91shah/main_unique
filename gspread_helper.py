588580a71714c348baabaa43c447a2c970408856
a91shah <17akshayshah@gmail.com>
2025-07-20 17:17:48 +0530

    Update gspread_helper.py

--- gspread_helper.py
+++ gspread_helper.py
@@ -1,22 +1,23 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
 
 def connect_to_gsheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds",
              "https://www.googleapis.com/auth/spreadsheets",
              "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("google_creds.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name).sheet1
     return sheet
 
-def load_inventory_from_gsheet(sheet_name="Inventory_Data"):
+def load_inventory_from_gsheet(sheet_name="inv_check"):
     sheet = connect_to_gsheet(sheet_name)
     data = sheet.get_all_records()
     return pd.DataFrame(data)
 
-def save_inventory_to_gsheet(df, sheet_name="Inventory_Data"):
+def save_inventory_to_gsheet(df, sheet_name="inv_check"):
     sheet = connect_to_gsheet(sheet_name)
     sheet.clear()
     sheet.update([df.columns.values.tolist()] + df.values.tolist())
